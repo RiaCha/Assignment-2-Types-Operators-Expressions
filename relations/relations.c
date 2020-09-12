7c9a38e12dd3a25a3319a71c8752b5eb3367f80d
corynezin <cory.nezin@gmail.com>
2020-09-12 14:31:57 -0400

    add examples

--- relations/relations.c
+++ relations/relations.c
@@ -2,12 +2,15 @@
 Author:
 
 This program checks the following:
+* 10 equals 10?
+* 8 equals 010?
 * 16 equals 0x10?
 * 259 equals 0x103?
 * 159 equals 0303?
 * 255 equals 0xff?
 * -1 equals 0xffff?
 * -1 equals 0xffffffff?
+* -1 equals 0xffffffffU?
 
 * -1 greater than 1U?
 
@@ -18,5 +21,7 @@ Print the question (copied from above) and the answer as a 0 or 1
 #include <stdio.h> /* printf */
 
 int main () {
-
+    printf("5 == 5: %d\n", 5 == 5);
+    printf("8 == 010: %d\n", 8 == 010);
+    printf("16 == 0x10: %d\n", 16 == 0x10);
 }
